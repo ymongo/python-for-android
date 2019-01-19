75c4d29478b5bc54b72e5def7238d83a6236fe76
ymongo <ymongo@hotmail.com>
2019-01-19 08:46:07 +0100

    test new url

--- pythonforandroid/recipes/sdl2_ttf/__init__.py
+++ pythonforandroid/recipes/sdl2_ttf/__init__.py
@@ -2,8 +2,10 @@ from pythonforandroid.recipe import BootstrapNDKRecipe
 
 
 class LibSDL2TTF(BootstrapNDKRecipe):
-    version = '2.0.14'
-    url = 'https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-{version}.tar.gz'
+    version = '2.0.15'
+    # url = 'https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-{version}.tar.gz'
+    # test mongo
+    url = 'https://www.libsdl.org/tmp/SDL_ttf/release/SDL2_ttf-{version}.tar.gz'
     dir_name = 'SDL2_ttf'
 
 
